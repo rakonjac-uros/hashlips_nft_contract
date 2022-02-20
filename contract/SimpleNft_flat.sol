9a1c50fea130512b67d81e77a321ce88ef84af31
eUros96 <ukacuki@hotmail.com>
2022-02-20 14:19:33 +0100

    MHV contract add

--- contract/SimpleNft_flat.sol
+++ contract/SimpleNft_flat.sol
@@ -1232,15 +1232,15 @@ abstract contract Ownable is Context {
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract NFT is ERC721Enumerable, Ownable {
+contract MHVTest is ERC721Enumerable, Ownable {
   using Strings for uint256;
 
   string baseURI;
   string public baseExtension = ".json";
-  uint256 public cost = 0.05 ether;
-  uint256 public maxSupply = 10000;
-  uint256 public maxMintAmount = 20;
-  bool public paused = false;
+  uint256 public cost = 0.04 ether;
+  uint256 public maxSupply = 3333;
+  uint256 public maxMintAmount = 5;
+  bool public paused = true;
   bool public revealed = false;
   string public notRevealedUri;
 
